f30adaa28a43c82dae8fd52becdb93c0930d83a0
NS <lab03@example.com>
2020-04-20 13:44:52 +0300

    code: первый тест

--- main.cpp
+++ main.cpp
@@ -14,23 +14,6 @@ vector<double> input_numbers(size_t count)
     return result;
 }
 
-void find_minmax(vector<double> numbers, double& min, double& max)
-{
-    min = numbers[0];
-    max = numbers[0];
-    for (double number : numbers)
-    {
-        if (number < min)
-        {
-            min = number;
-        }
-        if (number > max)
-        {
-            max = number;
-        }
-    }
-}
-
 vector <size_t> make_histogram(vector <double> numbers,size_t bin_count,double min,double max)
 {
     vector<size_t> bins(bin_count);
