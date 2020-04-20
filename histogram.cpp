6959cc29defc48b78a28b70e9813470206ebb844
NS <lab03@example.com>
2020-04-20 15:48:35 +0300

    code: добавлена новая проверка

--- histogram.cpp
+++ histogram.cpp
@@ -3,6 +3,10 @@
 void find_minmax();
 void find_minmax(vector<double> numbers, double& min, double& max)
 {
+    if(numbers.size() !=0)
+    {
+
+
     min = numbers[0];
     max = numbers[0];
     for (double number : numbers)
@@ -15,5 +19,6 @@ void find_minmax(vector<double> numbers, double& min, double& max)
         {
             max = number;
         }
+    }
     }
 }
