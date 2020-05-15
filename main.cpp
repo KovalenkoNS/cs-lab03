7ce7a6616001c1cf590f766bc7a09c3010bfe1a9
NS <lab03@example.com>
2020-05-15 13:55:10 +0300

    code: изменения функции input_numbers

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 #include "histogram_new.h"
 
 using namespace std;
-vector<double> input_numbers(size_t count)
+vector<double> input_numbers(istream& in ,size_t count)
 {
     vector<double> result(count);
     for (size_t i = 0; i < count; i++)
@@ -71,7 +71,7 @@ int main()
     cerr << "Enter number count: ";
     cin >> number_count;
     cerr << "Enter numbers: ";
-    const auto numbers=input_numbers(number_count);
+    const auto numbers=input_numbers(cin,number_count);
     size_t bin_count;
     cerr << "Enter column count: ";
     cin >> bin_count;
