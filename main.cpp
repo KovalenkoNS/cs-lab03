19c66475a7fb08f7d9bb21a90ed80c08deccb538
NS <lab03@example.com>
2020-05-15 15:55:59 +0300

    code: выполнение пункта 3.2.6

--- main.cpp
+++ main.cpp
@@ -94,17 +94,18 @@ int main(int argc, char* argv[])
 {
     if (argc>1)
     {
-        cout << "argc=" << argc<<endl;
-        cout<<"argv[0]="<<argv[0];
-        return(0);
+        CURL *curl = curl_easy_init();
+        if(curl) {
+            CURLcode res;
+            curl_easy_setopt(curl, CURLOPT_URL, argv[1]);
+            res = curl_easy_perform(curl);
+            curl_easy_cleanup(curl);
+        }
+        return 0;
     }
-    else
-    {
     curl_global_init(CURL_GLOBAL_ALL);
     const auto input = read_input(cin, true);
     const auto bins = make_histogram(input);
     show_histogram_svg(bins);
-
     return 0;
-    }
 }
