0a6ab025e55b9e1f90e2cd07869ec8851aa94a3b
NS <lab03@example.com>
2020-05-18 15:17:52 +0300

    Revert "Revert "Revert "code: выполнение пункта 3.1"""
    This reverts commit 28b2f6124ce48f7c0db6a56cbf80a140ee4c78ee.

--- main.cpp
+++ main.cpp
@@ -2,7 +2,6 @@
 #include <vector>
 #include "histogram.h"
 #include "histogram_new.h"
-#include <cstdio>
 
 using namespace std;
 vector<double> input_numbers(size_t count)
@@ -68,14 +67,6 @@ void show_histogram_text(vector<size_t>bins,size_t number_count)
 }
 int main()
 {
-
-    const char* name = "Commander Shepard";
-    int year = 2154;
-    printf("%s was born in %d.\n", name, year);
-    printf("n = %08x\n", 0x1234567);
-    return 0;
-
-
     size_t number_count;
     cerr << "Enter number count: ";
     cin >> number_count;
