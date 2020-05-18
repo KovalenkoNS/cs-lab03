2da00a5bf8ea2914198d8107703a5a00ba024ce0
NS <lab03@example.com>
2020-05-18 15:08:34 +0300

    code: выполнение пункта 3.1

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include "histogram.h"
 #include "histogram_new.h"
+#include <cstdio>
 
 using namespace std;
 vector<double> input_numbers(size_t count)
@@ -67,6 +68,14 @@ void show_histogram_text(vector<size_t>bins,size_t number_count)
 }
 int main()
 {
+
+    const char* name = "Commander Shepard";
+    int year = 2154;
+    printf("%s was born in %d.\n", name, year);
+    printf("n = %08x\n", 0x1234567);
+    return 0;
+
+
     size_t number_count;
     cerr << "Enter number count: ";
     cin >> number_count;
